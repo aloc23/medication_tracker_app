d96644c8442536af328a81b0fc8c2a9fe85e4731
Alan Clarke <alannoelc@gmail.com>
2025-07-21 21:39:12 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,37 @@
+const CACHE_NAME = 'med-tracker-cache-v1';
+const toCache = [
+  './',
+  './index.html',
+  './user.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || "Medication Reminder";
-  const options = {
-    body: data.body || "It's time to take your medication.",
-    icon: 'icon-192.png',
-    badge: 'icon-192.png'
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(toCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
